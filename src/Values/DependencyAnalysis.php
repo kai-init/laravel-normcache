aadd7a914d6486ecc2d8a08913486b289d08b789
Kai H <32325424+kai-init@users.noreply.github.com>
2026-09-23 14:36:33 +1000

    restore dependency analysis value object

--- src/Values/DependencyAnalysis.php
+++ src/Values/DependencyAnalysis.php
@@ -4,4 +4,11 @@ namespace NormCache\Values;
 
 final readonly class DependencyAnalysis
 {
+    /** @param list<TableIdentity> $tables */
+    public function __construct(
+        public ?TableIdentity $root,
+        public array $tables,
+        public bool $queryScoped,
+        public ?string $bypassReason,
+    ) {}
 }
