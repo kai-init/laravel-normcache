ff060ccc43bf47b394f3f1397e106e4d8c2574ce
Kai H <32325424+kai-init@users.noreply.github.com>
2026-09-23 14:35:20 +1000

    remove redundant dependency helper

--- src/Values/DependencyAnalysis.php
+++ src/Values/DependencyAnalysis.php
@@ -4,23 +4,4 @@ namespace NormCache\Values;
 
 final readonly class DependencyAnalysis
 {
-    /** @param list<TableIdentity> $tables */
-    public function __construct(
-        public ?TableIdentity $root,
-        public array $tables,
-        public bool $queryScoped,
-        public ?string $bypassReason,
-    ) {}
-
-    /** @param list<TableIdentity> $tables */
-    public static function hasExternalTo(TableIdentity $root, array $tables): bool
-    {
-        foreach ($tables as $table) {
-            if ($table->hash !== $root->hash) {
-                return true;
-            }
-        }
-
-        return false;
-    }
 }
